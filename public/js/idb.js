10cd7936327f8b476569f8d0fb4adf98ea64acd4
bortkyle18 <bortkyle18@gmail.com>
2022-08-11 14:53:48 -0400

    project upload

---
+++ public/js/idb.js
@@ -0,0 +1,68 @@
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+let db;
+
+
+const database = "budget"
+const objectStore = "transactions"
+
+const request = indexedDB.open(database, 1);
+
+request.onupgradeneeded = ({ target }) => {
+  let db = target.result;
+  db.createObjectStore(objectStore, { autoIncrement: true });
+};
+
+request.onsuccess = ({ target }) => {
+  db = target.result;
+  // check if app is online before reading from db
+  if (navigator.onLine) {
+    checkDatabase();
+  }
+};
+
+request.onerror = function(event) {
+  console.log("Woops! " + event.target.errorCode);
+};
+function saveRecord(record) {
+  const transaction = db.transaction(["transactions"], "readwrite");
+  const store = transaction.objectStore("transactions");
+  store.add(record);
+}
+
+function checkDatabase() {
+  const transaction = db.transaction(["transactions"], "readwrite");
+  const store = transaction.objectStore("transactions");
+  const getAll = store.getAll();
+
+  getAll.onsuccess = function() {
+    if (getAll.result.length > 0) {
+
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json"
+        }
+      })
+      .then(response => {        
+        return response.json();
+      })
+      .then(() => {
+        // delete indexedDB records if the update is successful
+        const transaction = db.transaction([objectStore], "readwrite");
+        const store = transaction.objectStore(objectStore);
+        store.clear();
+      });
+    }
+  };
+}
+
+// listen for app coming back online
+window.addEventListener("online", checkDatabase);
